2155bf3819e897184a131fb273f7ebd6c3f89662
Micaela Cunha <micaelacunhadev@gmail.com>
2024-01-27 18:23:02 -0500

    Day 19: Partial Solution to Unreal Webcam Fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,32 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+      .then(localMediaStream =>{
+        console.log(localMediaStream, typeof localMediaStream)
+        video.srcObject = localMediaStream;
+        video.play();
+      })
+      .catch(err => console.error("Oh no!!", err));
+}
+
+
+function paintToCanvas(){
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log('width', width, 'height', height)
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() =>{
+    ctx.drawImage(video,0,0, width, height)
+
+  }, 16)
+
+}
+
+paintToCanvas();
+getVideo()
