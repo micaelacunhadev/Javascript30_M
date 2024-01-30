49e465205211db7b164c2c8534b25c598d2b5a5e
Micaela Cunha <micaelacunhadev@gmail.com>
2024-01-29 20:53:16 -0500

    Day 21: Solution to Geolocation Based Speedometer and Compass, and finished Day 19 solution

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -23,12 +23,55 @@ function paintToCanvas(){
   canvas.width = width;
   canvas.height = height;
 
-  setInterval(() =>{
+  return setInterval(() =>{
     ctx.drawImage(video,0,0, width, height)
-
+    // take the pixes out
+    let pixels = ctx.getImageData(0,0,width,height)
+    // mess with them
+    //pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // put them back
+    ctx.putImageData(pixels,0,0)
+    
   }, 16)
+}
+
+
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');  
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src=${data} alt="handsome person" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+
+function redEffect(pixels) {
+  for (let i=0; i < pixels.data.length; i+=4){
+    pixels.data[i] += 100;
+    pixels.data[i+1] -= 50;
+    pixels.data[i+2] *= 0.5;
+  }
+  return pixels;
 
 }
 
-paintToCanvas();
-getVideo()
+function rgbSplit(pixels){
+   for (let i=0; i < pixels.data.length; i+=4){
+    pixels.data[i-150] = pixels.data[i]; // red
+    pixels.data[i+100] = pixels.data[i+1]; // green
+    pixels.data[i-150] = pixels.data[i+2]; // blue
+  }
+  return pixels;
+}
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas)
